f01e8c96e17fe335570bd5804af2e14e4625f3fa
Fabio-Miguel-Andrade <102106802+Fabio-Miguel-Andrade@users.noreply.github.com>
2025-06-18 12:29:00 -0300

    Update Exercicio5.c

--- Exercicio5.c
+++ Exercicio5.c
@@ -2,18 +2,18 @@
 
 int main(){
     //Vaiáveis
-    float vet[4], inv[4];
+    float vet[10], inv[10];
     int i, j;
 
     //Declaração de VAR (Entrada de DADOS)
-    for(i = 0; i < 4; i++){
+    for(i = 0; i < 10; i++){
         printf("\nDigite um numero: ");
         scanf("%f", &vet[i]);
     }
 
     //Lógica
-    for(i = 0; i < 4; i++){
-        for(j = 3; j >= 0; j--){
+    for(i = 0; i < 10; i++){
+        for(j = 9; j >= 0; j--){
             inv[i] = vet[j];
             printf("\n%.2f", inv[i]);
         }
